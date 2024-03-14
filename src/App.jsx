20d235ef10801b1fafa732613cd2223fbeee11d8
CliffDawen <zdawen9@gmail.com>
2024-03-14 22:28:18 +1100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { gql, useQuery } from '@apollo/client'
+
+const ALL_PERSONS = gql`
+query {
+  allPersons {
+    name
+    phone
+    id
+  }
+}
+`
+
+const App = () => {
+  const result = useQuery(ALL_PERSONS)
+
+  if (result.loading) {
+    return <div>loading...</div>
+  }
+  if (result.loading) {
+    return <div>loading...</div>
+  }
+
+  if (result.error) {
+    return <div>Error: {result.error.message}</div>;
+  }
+  
+  if (!result.data) {
+    return <div>No data available</div>;
+  }
+
+  return (
+    <div>
+      {result.data.allPersons.map(p => p.name).join(', ')}
+    </div>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+import ReactDOM from 'react-dom/client'
+import App from './App'
+
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+} from '@apollo/client'
+const cors = require('cors');
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000', // <- this could be the issue if not set correctly
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+)
